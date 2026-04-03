033b4a89ed7d4cc8514d58e562ba64d2263f27cf
haiDuong <haid9614@gmail.com>
2026-04-03 17:23:46 +0700

    03/04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
-
 import org.hibernate.Session;
 import  org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // 1. Đọc cấu hình từ file hibernate.cfg.xml
@@ -14,23 +14,79 @@ public class Main {
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
-            RestaurantTable table1 = new RestaurantTable("vip", 10, "trong");
-            RestaurantTable table2 = new RestaurantTable("thuong", 5, "trong");
-            RestaurantTable table3 = new RestaurantTable("ngoai_san", 4, "trong");
-            session.persist(table1);
-            session.persist(table2);
-            session.persist(table3);
+            RestaurantTable table3 = session.get(RestaurantTable.class, 3);
+            if(table3 != null) {
+                session.remove(table3);
+                System.out.println(" DA XOA BAN THANH CONG !!!");
+            } else {
+                System.out.println(" XOA BAN KHONG THANH CONG!!!");
+            }
             transaction.commit();
-            System.out.println(" DA LUU XUONG DATABASE THANH CONG !!!!!!!!!!");
-        } catch (Exception e) {
+
+        } catch(Exception e) {
             if(transaction != null) {
                 transaction.rollback();
             }
             e.printStackTrace();
-        } finally {
+        }finally {
             session.close();
             factory.close();
         }
+//        try {
+//            transaction = session.beginTransaction();
+//            RestaurantTable tableVip = session.get(RestaurantTable.class, 1);
+//            if(tableVip != null) {
+//                tableVip.setIs_available("coKhach");
+//                System.out.println(" da updat trang thai thanh cong !!!");
+//            } else {
+//                System.out.println("khong thanh cong");
+//            }
+//            transaction.commit();
+//        } catch (Exception e) {
+//              transaction.rollback();
+//              e.printStackTrace();
+//        } finally {
+//             session.close();
+//             factory.close();
+//        }
+
+//        try {
+//            transaction = session.beginTransaction();
+//            String hql = "from RestaurantTable where is_available = 'trong'";
+//            List<RestaurantTable> list = session.createQuery(hql, RestaurantTable.class).getResultList();
+//            System.out.println("DANH SACH CAC BAN TRONG LA");
+//            for(RestaurantTable table : list) {
+//                System.out.println("id ban: " + table.getId() + " | ten ban: " + table.getTable_name() + " | so luong nguoi: " + table.getCapacity());
+//            }
+//            transaction.commit();
+//        } catch (Exception e) {
+//            if(transaction != null) {
+//                transaction.rollback();
+//            }
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//            factory.close();
+//        }
+        // try {
+        //     transaction = session.beginTransaction();
+        //     RestaurantTable table1 = new RestaurantTable("vip", 10, "trong");
+        //     RestaurantTable table2 = new RestaurantTable("thuong", 5, "trong");
+        //     RestaurantTable table3 = new RestaurantTable("ngoai_san", 4, "trong");
+        //    session.persist(table1);
+        //    session.persist(table2);
+        //    session.persist(table3);
+        //    transaction.commit();
+        //    System.out.println(" DA LUU XUONG DATABASE THANH CONG !!!!!!!!!!");
+        // } catch (Exception e) {
+        //    if(transaction != null) {
+        //        transaction.rollback();
+        //    }
+        //    e.printStackTrace();
+        // } finally {
+        //    session.close();
+        //    factory.close();
+        // }
         // try {
             // Bắt đầu một giao dịch (Transaction) - Bắt buộc khi muốn thay đổi dữ liệu (Insert/Update/Delete)
          //   transaction = session.beginTransaction();
